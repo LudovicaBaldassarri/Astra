ab394d26513db2059b8d6defadc813003e7d7dd8
lukehalfhead <mezzatestaluca@gmail.com>
2016-12-15 16:15:14 +0100

    Miglioramenti ASTRA ed ENERGY SPHERE
    ASTRA:
    - Aggiunto il raycasting dalla telecamera
    
    ENERGY SPHERE:
    - La palla di energia scompare quando ASTRA la attraversa
    - Creato un prefab univoco
    - Migliorie varie per la sfera di energia

---
+++ Assets/Scripts/Astra.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Astra : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		RaycastHit hit;
+		Ray ray = new Ray (transform.position, transform.forward);
+
+
+		if (Physics.Raycast (ray, out hit, 100.0f)) {
+			print ("Found object (" + hit.collider.gameObject.name + ") distance: " + hit.distance);
+			Debug.DrawRay (transform.position, forward);
+		}
+	}
+}

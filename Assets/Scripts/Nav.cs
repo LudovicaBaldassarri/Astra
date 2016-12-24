55417afc3f8d277580f919d096264cfc6fd6437a
lukehalfhead <mezzatestaluca@gmail.com>
2016-12-24 17:14:22 +0100

    NAV si unisce alla famiglia!
    - Aggiunto NAV con la propria texture
    - Modificato il materiale dei pannelli laterali e del proiettore di NAV
    - Aggiunto il collider esterno di nav ma non alle sue porte (per DEBUG)
    - Aggiunto uno script che fa aprire le porte quando ASTRA passa per un
    collider attorno a NAV

---
+++ Assets/Scripts/Nav.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nav : MonoBehaviour {
+
+	private bool closed = true;
+	private bool anim = false;
+	private float degrees = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (anim) {
+
+			int sign = 1;
+
+			// FIXME: rendilo framrate independent
+			if (closed) {
+				degrees++;
+				sign = 1;
+			} else {
+				degrees--;
+				sign = -1;
+			}
+
+			// TODO: do the animation here
+
+//			for (int i = 0; i < this.transform.childCount - 1; i++) {
+//				if (this.transform.GetChild (i).transform.name == "Nav_portaDx") {
+//					this.transform.GetChild(i).transform.Rotate(new Vector3(0.0f,0.0f,sign*-1f));
+//				} else if (this.transform.GetChild (i).transform.name == "Nav_PortaSx") {
+//					this.transform.GetChild(i).transform.Rotate(new Vector3(0.0f,0.0f,sign*1f));
+//				}
+//			}
+			this.transform.GetChild(1).transform.Rotate(new Vector3(0.0f,0.0f,sign*-1f));
+			this.transform.GetChild(2).transform.Rotate(new Vector3(0.0f,0.0f,sign*1f));
+
+			if (degrees >= 90 || degrees <= 0) {
+				anim = false;
+				closed = !closed;
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider collision) {
+		anim = true;
+	}
+}

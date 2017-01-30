cae35ae6e756fd6a4108c4bf6d2c8c87d0e90dfa
lukehalfhead <mezzatestaluca@gmail.com>
2017-01-30 21:58:12 +0100

    La morte e la coda di Lumen
    - Se Astra cade dentro un cratere, si fa male e rinasce all’inizio del
    livello
    - Aggiunto un effetto particellare per la coda di Lumen
    - Ingrandito il collider del potassio per migliorarne il puntamento
    
    Bug noti:
    Quando lumen si ferma, scompare!

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+
+		if (other.CompareTag ("Player")) {
+			// Riporta ASTRA all'inizio del gioco
+			// TODO: Da cambiare!
+			other.gameObject.transform.position = new Vector3(-26,18,89);
+		}
+	}
+}

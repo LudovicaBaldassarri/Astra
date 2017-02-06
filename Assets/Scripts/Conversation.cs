c7f035fa2c50b9873f6184800f10c6632e98e8d7
lukehalfhead <mezzatestaluca@gmail.com>
2017-02-06 21:59:54 +0100

    Aggiunto sistema per dialoghi
    Aggiunto un package per implementare i dialoghi. Ancora non testato

---
+++ Assets/Scripts/Conversation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conversation : MonoBehaviour {
+
+	[System.Serializable]
+	public class Node {
+		public string dialogueText;
+		public Node[] responses;
+	}
+
+	public Node startNode;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

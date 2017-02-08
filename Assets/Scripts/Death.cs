64410254f719e6352b8307c15ae3b7201064ac14
lukehalfhead <mezzatestaluca@gmail.com>
2017-02-08 18:25:03 +0100

    Montacarichi e Apollo 11
    - Aggiunto box collider alla stazione del livello 3
    - Aggiunta un’altra porzione di nebbia al livello 3
    - Risolto problema materiale del montacarichi livello 3
    - Aggiunto il death collider livello 3
    - Aggiunto montacarichi livello 3
    - Modificato lo script per la morte, aggiunta la possibilità di
    stabilire il punto di respawn
    - Aggiunto Apollo livello 1

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Death : MonoBehaviour {
 
+	public float respawnX;
+	public float respawnY;
+	public float respawnZ;
+
 	void OnTriggerEnter(Collider other) {
 
 		if (other.CompareTag ("Player")) {
 			// Riporta ASTRA all'inizio del gioco
 			// TODO: Da cambiare!
-			other.gameObject.transform.position = new Vector3(-26,18,89);
+			other.gameObject.transform.position = new Vector3(respawnX,respawnY,respawnZ);
 		}
 	}
 }
